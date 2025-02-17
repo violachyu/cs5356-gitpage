e1ff338ff51d5bf5749bd6f1ab894dc9f7cbf184
violachyu <pinckychyu@gmail.com>
2025-02-17 17:13:01 -0500

    Job Type Filter w/ Mouse event

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
+    fetchData()
+});
+
+// Job Type Filter Behavior
+document.getElementsByName('checkbox').forEach(checkbox => {
+    checkbox.addEventListener('click', function() { // mouse event
+        document.getElementsByName('checkbox').forEach(item => {
+            if (item !== this){
+                item.checked = false; // uncheck all other boxes
+            } else {
+                query = item.id;
+                console.log(query)
+                fetchData(query);
+            }
+        });
+    });
+});
+
+
+
+/* Util */
+function fetchData(query='') {
     const jobList = document.getElementById("job-list");
     const timestamp = document.getElementsByTagName("time")[0];
-    console.log(timestamp);
 
-    fetch("https://jobicy.com/api/v2/remote-jobs?count=20&geo=usa&industry=engineering")
+    fetch(`https://jobicy.com/api/v2/remote-jobs?geo=usa&industry=engineering`)
         .then(response => response.json())
         .then(data => {
             jobList.innerHTML = ""; // Clear loading message
             timestamp.innerHTML = data.lastUpdate;
-            data.jobs.forEach(job => {
+
+            let filterResults = data.jobs.filter(job =>{ 
+                return job.jobTitle.toLowerCase().includes(query.toLowerCase())
+            });
+
+            filterResults.forEach(job => {
                 const row = document.createElement("tr");
                 row.innerHTML = `
                     <td>${job.jobTitle}</td>
@@ -24,4 +50,4 @@ document.addEventListener("DOMContentLoaded", function () {
             jobList.innerHTML = "<tr><td colspan='4'>Failed to load jobs.</td></tr>";
             console.error("Error fetching job listings:", error);
         });
-});
+}
